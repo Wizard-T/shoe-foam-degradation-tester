2a8a449b5bf472b398da3177514596b6f14e8468
Wizard-T <templincarson@gmail.com>
2026-08-11 21:59:27 -0400

    add v1_prototype firmware
    Drives stepper through advance/retract cycle, reads force via HX711,
    logs displacement + force over serial. Homing and calibration still
    needed

--- firmware/v1_prototype/v1_prototype.ino
+++ firmware/v1_prototype/v1_prototype.ino
@@ -28,12 +28,94 @@
   ============================================================
 */
 
+#include <HX711.h>
+
+HX711 platform;
+
+float verticalIncrement = 0.1; //in mm
+float displacement = 0.0;
+const int STEPS_PER_REV = 200;
+const int MICROSTEPS_PER_STEP = 8;  // may change based on MS1/MS2 wiring from board to board
+const float LEAD_SCREW_PITCH_MM = 2.0;
+const float MICROSTEPS_PER_MM = (STEPS_PER_REV * MICROSTEPS_PER_STEP) / LEAD_SCREW_PITCH_MM;
+float microsteps = verticalIncrement * MICROSTEPS_PER_MM;
+
+  /*
+  Lead screw pitch is 2mm, meaning rotating the lead screw once will move the compressor 2mm
+  TMC2209 defaults to 8 microsteps (MS1/MS2 unconfigured, both at GND), meaning writing HIGH to the pin produces a microstep and 8 microsteps make 1 step
+  1 step/8 microsteps rotates the motor 1.8 degrees
+  1 microstep rotates the motor 0.225 degrees
+  200 steps/rev
+  1600 microsteps/rev
+  1 step = 1/100 mm
+  1 microstep = 1/800 mm
+  */
+
+//pinout
+const int DOUT_PIN = 2;
+const int SCK_PIN = 3;
+const int STEP_PIN = 4;
+const int DIR_PIN = 5;
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  platform.begin(DOUT_PIN, SCK_PIN);
+  platform.tare();
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  float force = readForceGrams();
+  digitalWrite(DIR_PIN, HIGH);
+  delayMicroseconds(500);
+ 
+  while(force<10000){
+    advancePlatform();  
+    force = readForceGrams();
+    Serial.print(displacement);
+    Serial.print(",");
+    Serial.println(force);
+  }
+
+  digitalWrite(DIR_PIN, LOW);
+  delayMicroseconds(500);
+
+  while(displacement>0){
+    retractPlatform();  
+    force = readForceGrams();
+    Serial.print(displacement);
+    Serial.print(",");
+    Serial.println(force);
+  }
+  
+
+}
+
+float readForceGrams(){
+  return platform.get_units(3);
+}
+
+void advancePlatform(){
+  for(int i=0; i<microsteps; i++){
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(500);
+  }
+  displacement += verticalIncrement;
+}
+
+void retractPlatform(){
+  for(int i=0; i<microsteps; i++){
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(500);
+  }
+  displacement -= verticalIncrement;
 }
